36008f75157f595613fff75904131208718e88e0
avinashluhana <aviluhana@gmail.com>
2022-02-09 21:58:18 +0500

    added parameters

--- customEvents.js
+++ customEvents.js
@@ -4,3 +4,9 @@ const events = new event.EventEmitter();
 
 events.on("clickEvent", () => console.log("event fired"));
 events.emit("clickEvent");
+
+
+// event with parameters
+
+events.on("eventWithParameters", (para, check)=>console.log(para, check));
+events.emit("eventWithParameters","parameter event fired","checking")
